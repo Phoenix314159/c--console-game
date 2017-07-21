c85b4a7bca0bfbc4a6ed838680f936c955d99b8b
James Langford <derivative7117@live.com>
2017-07-21 13:32:46 -0600

    completed console game

--- NumberWizard.cs
+++ NumberWizard.cs
@@ -3,34 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+	int max;
+	int min;
+	int guess;
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = 500;
+		print ("========================");
 		print ("Welcome to Number Wizard");
 		print ("Pick a Number in your head but don't tell me!");
 		print ("Only pick a whole number!");
-
-		int max = 1000;
-		int min = 1;
-
 		print ("The highest number you can pick is " + max + ".");
 		print ("The lowest number you can pick is " + min + ".");
-
-		print ("Is the number higher or lower than 500?");
+		print ("Is the number higher or lower than " + guess + "?");
 		print ("Up = higher, down = lower, return = equal");
-
-
+		max = max + 1;
+	}
+	void NextGuess() {
+		guess = (max + min) / 2;
+		print ("Higher or lower than " + guess);
+		print ("Up = higher, down = lower, return = equal");
 	}
 
-	// Update is called once per frame
+	void Start () {
+		StartGame();
+	}
+
 	void Update () {
-
-		if(Input.GetKeyDown(KeyCode.UpArrow)) {
-			print("Up arrow pressed");
-		}
-
-		if(Input.GetKeyDown(KeyCode.DownArrow)) {
-			print("Down arrow pressed");
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			min = guess;
+			NextGuess();
+		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			max = guess;
+			NextGuess();
+		}else if (Input.GetKeyDown(KeyCode.Return)) {
+			print("I won!");
+			StartGame ();
 		}
 	}
 }
